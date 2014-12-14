95a9eb0b3c9a9af6e9c75476d0ddd0c0984891a8
Nicolas Kramer <nicolas@kramerfloyd.com>
2014-12-14 10:39:38 +0100

    gaussian clusters function added

---
+++ gaussian_distributed_clusters.py
@@ -0,0 +1,20 @@
+__author__ = 'nicolas'
+import numpy as np
+import random
+
+
+def init_board_gauss(N, k):
+    n = float(N)/k
+    ar = []
+    for i in range(k):
+        c = (random.uniform(-1, 1), random.uniform(-1, 1))
+        s = random.uniform(0.05,0.5)
+        x = []
+        while len(x) < n:
+            a, b = np.array([np.random.normal(c[0], s), np.random.normal(c[1], s)])
+            # Continue drawing points from the distribution in the range [-1,1]
+            if abs(a) < 1 and abs(b) < 1:
+                x.append([a,b])
+        ar.extend(x)
+    ar = np.array(ar)[:N]
+    return ar
