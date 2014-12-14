b6e5b9890e5ac40aa33061b44482b84ac8369766
Nicolas Kramer <nicolas@kramerfloyd.com>
2014-12-14 17:05:48 +0100

    Working plot of gauss disstribution with k clusters

---
+++ dcl_code.py
@@ -0,0 +1,74 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+
+def cluster_points(points, mu):
+    clusters = {}
+    for x in points:
+        best_mu_key = min([(i[0], np.linalg.norm(x-mu[i[0]])) \
+                    for i in enumerate(mu)], key=lambda t:t[1])[0]
+        try:
+            clusters[best_mu_key].append(x)
+        except KeyError:
+            clusters[best_mu_key] = [x]
+    return clusters
+
+
+def reevaluate_centers(mu, clusters):
+    newmu = []
+    keys = sorted(clusters.keys())
+    for k in keys:
+        newmu.append(np.mean(clusters[k], axis = 0))
+    return newmu
+
+
+def has_converged(mu, oldmu):
+    return set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu])
+
+
+def find_centers(x, k):
+    # Initialize to K random centers
+    oldmu = random.sample(x, k)
+    mu = random.sample(x, k)
+    while not has_converged(mu, oldmu):
+        oldmu = mu
+        # Assign all points in X to clusters
+        clusters = cluster_points(x, mu)
+        # Reevaluate centers
+        mu = reevaluate_centers(oldmu, clusters)
+    return(mu, clusters)
+
+
+def init_board(n):
+    ar = np.array([(random.uniform(-1, 1), random.uniform(-1, 1)) for i in range(n)])
+    return ar
+
+
+def init_board_gauss(n, k):
+    n = float(n)/k
+    ar = []
+    for i in range(k):
+        c = (random.uniform(-1, 1), random.uniform(-1, 1))
+        s = random.uniform(0.05, 0.5)
+        x = []
+        while len(x) < n:
+            a, b = np.array([np.random.normal(c[0], s), np.random.normal(c[1], s)])
+            # Continue drawing points from the distribution in the range [-1,1]
+            if abs(a) < 1 and abs(b) < 1:
+                x.append([a, b])
+        ar.extend(x)
+    ar = np.array(ar)[:n]
+    return ar
+
+X = init_board_gauss(200, 2)
+xs = []
+ys = []
+
+for i in X:
+    xs.append(i[0])
+    ys.append(i[1])
+
+plt.plot(xs, ys, 'ro')
+plt.axis([-1, 1, -1, 1])
+plt.show()
